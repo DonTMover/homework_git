c026ccd15749ed2b049aa61219011f19bfafd034
DonTMover <antantov624@gmail.com>
2024-08-05 19:18:43 +0300

    added method Some

---
+++ src/gg/dontmover/homework_git/Main.java
@@ -0,0 +1,10 @@
+package gg.dontmover.homework_git;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    private static void Some(){
+        System.out.printf("Hello World\n");
+    }
+}
