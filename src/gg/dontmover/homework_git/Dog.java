5818b93b61fa869afc89b6f1149ef5244bfd9205
Egor <46960051+DonTMover@users.noreply.github.com>
2024-08-06 13:19:00 +0300

    Create Dog.java

---
+++ src/gg/dontmover/homework_git/Dog.java
@@ -0,0 +1,10 @@
+package gg.dontmover.homework_git;
+
+public class Dog {
+    private String name;
+    private int age;
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
