66f0df2b00362876d8fbeb7ff8f103017c2bdb52
Egor <46960051+DonTMover@users.noreply.github.com>
2024-08-06 13:11:55 +0300

    Update Main.java

--- src/gg/dontmover/homework_git/Main.java
+++ src/gg/dontmover/homework_git/Main.java
@@ -2,7 +2,7 @@ package gg.dontmover.homework_git;
 
 public class Main {
     public static void main(String[] args) {
-
+        Some();
     }
     private static void Some(){
         System.out.printf("Hello World\n");
